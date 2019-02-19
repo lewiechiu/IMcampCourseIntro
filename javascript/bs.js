c238ea6fbbc46406aa0f45286fea19cc72b2827b
lewiechiu <louiechiu@gmail.com>
2019-02-19 23:31:56 +0800

    all done

--- javascript/bs.js
+++ javascript/bs.js
@@ -11,12 +11,31 @@ $(function () {
 $(document).ready(function(){
   console.log(123);
   var owl = $('.owl-carousel');
-  owl.owlCarousel({
+  if ($(window).width() < 960){
+      owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplaySpeed:1500,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+    $('.picture-course').css({
+      "font-weight":"300",
+      "font-size":"20px"
+    });
+  }
+  else{
+    owl.owlCarousel({
       items:3,
       loop:true,
       margin:10,
       autoplay:true,
+      autoplaySpeed:2500,
       autoplayTimeout:3000,
       autoplayHoverPause:true
   });
+  }
+  
 });

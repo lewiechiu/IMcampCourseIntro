f859980a33f74ee692b9fc243d72afe4b711bddf
lewiechiu <louiechiu@gmail.com>
2019-02-19 22:32:45 +0800

    added carousel in the related course

--- javascript/bs.js
+++ javascript/bs.js
@@ -4,3 +4,19 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+
+
+
+$(document).ready(function(){
+  console.log(123);
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items:3,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true
+  });
+});
